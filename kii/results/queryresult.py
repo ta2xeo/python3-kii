892da42601318bcc15e70378614be76d68681881
Tatsuji Tsuchiya <ta2xeo@gmail.com>
2016-03-22 15:15:07 +0900

    QueryResult uses cache_results

--- kii/results/queryresult.py
+++ kii/results/queryresult.py
@@ -7,52 +7,66 @@ from .object import ObjectResult
 class QueryResult(BaseResult):
     def __init__(self, request_helper, response=None):
         super().__init__(request_helper, response)
-        self._total_items = None
+        self._cache_results = []
+        self._finished = False
 
     @property
-    def total_items(self):
-        if self._total_items is None:
-            self._total_items = list(self.__iter__())
-        return self._total_items
+    def all_items(self):
+        if not self._finished:
+            self._cache_results = list(self.__iter__())
+            self._finished = True
+        return self._cache_results
 
     def __len__(self):
-        return len(self.total_items)
+        return len(self.all_items)
 
     def __getitem__(self, key):
-        return self.total_items[key]
+        return self.all_items[key]
 
     def __setitem__(self, key, val):
-        self.total_items[key] = val
+        self.all_items[key] = val
 
     def __delitem__(self, key):
-        self.total_items.pop(key)
+        self.all_items.pop(key)
 
     def __bool__(self):
-        return bool(self.total_items)
+        if not self._finished:
+            b = bool(self._cache_results)
+            if b:
+                return True
+        return bool(self.all_items)
 
     def pop(self, index=None):
         if index is None:
-            return self.total_items.pop()
+            return self.all_items.pop()
         else:
-            return self.total_items.pop(index)
+            return self.all_items.pop(index)
 
     def __iter__(self):
-        if self._total_items is not None:
-            for item in self._total_items:
+        if self._finished:
+            for item in self._cache_results:
                 yield item
             return
 
+        for item in self._cache_results[:]:
+            yield item
+
+        cache_offset = len(self._cache_results)
         count = -self.request_helper._offset
-        for item in self._items:
+        for item in self._items[cache_offset:]:
             count += 1
             if count <= 0:
                 continue
             if self.request_helper._limit and count > self.request_helper._limit:
+                self._finished = True
                 return
+
+            self._cache_results.append(item)
             yield item
 
         while self.next_pagination_key:
             if self.request_helper._limit and count >= self.request_helper._limit:
+                self._finished = True
                 return
 
             helper = self.request_helper.clone()
@@ -64,14 +78,18 @@ class QueryResult(BaseResult):
                     continue
 
                 if self.request_helper._limit and count > self.request_helper._limit:
+                    self._finished = True
                     return
 
+                self._cache_results.append(item)
                 yield item
 
             self.next_pagination_key = result.next_pagination_key
 
+        self._finished = True
+
     def json(self):
-        return [item.json() for item in self.total_items]
+        return [item.json() for item in self]
 
     def __str__(self):
         return json.dumps(self.json())
